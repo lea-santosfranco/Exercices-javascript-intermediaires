ef9904754ed90dcbd3ef902e8c0c4bd0c562e7ce
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-02-06 11:36:27 +0100

    exercice du feu tricolore mis en commentaire pour pas qu'il apparaisse à l'affichage avec live server début d'exercice sur la modale

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-function allumerFeu(){
-  var feuActuel = document.querySelector(".allume");
+// function allumerFeu(){
+//   var feuActuel = document.querySelector(".allume");
 
-  const rouge = document.getElementById("red");
-  const orange = document.getElementById("orange");
-  const vert = document.getElementById("green");
+//   const rouge = document.getElementById("red");
+//   const orange = document.getElementById("orange");
+//   const vert = document.getElementById("green");
 
-  feuActuel.classList.remove ("allume");
+//   feuActuel.classList.remove ("allume");
 
-  if(feuActuel.id === "red"){
-    orange.classList.add ("allume")
-  }
-  else if (feuActuel.id === "orange"){
-    vert.classList.add ("allume")
-  }
+//   if(feuActuel.id === "red"){
+//     orange.classList.add ("allume")
+//   }
+//   else if (feuActuel.id === "orange"){
+//     vert.classList.add ("allume")
+//   }
 
-  else {
-    rouge.classList.add ("allume")
-  }
-}
-setInterval (allumerFeu, 1000);
+//   else {
+//     rouge.classList.add ("allume")
+//   }
+// }
+// setInterval (allumerFeu, 1000);
+
+addEventListener(click) window 
+display none block   
+classList add remove  
+
+const buttonOpen = document.getElementById("buttonOpen");
+const buttonClose = document.getElementById("buttonClose");
