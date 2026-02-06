16677dbfa13891a4c98dea7b873f117edf87d627
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-02-06 09:58:41 +0100

    Ajout de l'exercice du feu tricolore

---
+++ index.js
@@ -0,0 +1,21 @@
+function allumerFeu(){
+  var feuActuel = document.querySelector(".allume");
+
+  const rouge = document.getElementById("red");
+  const orange = document.getElementById("orange");
+  const vert = document.getElementById("green");
+
+  feuActuel.classList.remove ("allume");
+
+  if(feuActuel.id === "red"){
+    orange.classList.add ("allume")
+  }
+  else if (feuActuel.id === "orange"){
+    vert.classList.add ("allume")
+  }
+
+  else {
+    rouge.classList.add ("allume")
+  }
+}
+setInterval (allumerFeu, 2000);
