a29457fea157fc81ab574ed2b6ec97b58e56c322
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-02-08 13:51:47 +0100

    ajout de l'exercice 4 et 5

--- index.js
+++ index.js
@@ -20,9 +20,60 @@
 // }
 // setInterval (allumerFeu, 1000);
 
-addEventListener(click) window 
-display none block   
-classList add remove  
+   // addEventListener(click) window 
+   // display none block   
+   // classList add remove  
 
-const buttonOpen = document.getElementById("buttonOpen");
-const buttonClose = document.getElementById("buttonClose");
+   // const modal = document.getElementById("myModal");
+   // const buttonOpen = document.getElementById("buttonOpen");
+   // const closeButton = document.querySelector(".closeButton");
+
+
+   //Exercice 4 //
+   class calculerSomme{
+    constructor(listeNombre){
+        this.nombres = listeNombre
+        this.somme = 0
+    }
+    calculer(){
+        let total = 0;
+        for (let i = 0; i < this.nombres.length; i++){
+            total = total + this.nombres[i]
+        }
+        this.somme = total;
+        return this.somme;
+    }
+    afficherResultat(){
+        console.log("Resultat :" + this.somme);
+    }
+   }
+
+//Exercice 5//
+
+class Arme{
+    constructor(nom,dégats, type){
+        this.nom = nom;
+        this.dégats = dégats;
+        this.type = type;
+    }
+    afficherInfos(){
+        console.log("Nom de l'arme :" + this.nom);
+        console.log("Dégats de l'arme :" + this.dégats);
+        console.log("Type de l'arme : " + this.type);
+    }
+}
+class Sort{
+    constructor(nom, puissance, coutMana, element){
+        this.nom = nom;
+        this.puissance = puissance;
+        this.coutMana = coutMana;
+        this.element = element;
+    }
+    afficherInfos(){
+        console.log("Nom du sort :" + this.nom);
+        console.log("Puissance du sort :" + this.puissance);
+        console.log("Coût en mana du sort : " + this.coutMana);
+        console.log("Element du sort : " + this.element);
+    }
+}
+
