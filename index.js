14a60e3cb7d8caf2c6aa3af4877a2734e9cea890
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-02-09 14:19:42 +0100

    avancement exercice 6

--- index.js
+++ index.js
@@ -43,21 +43,21 @@
 
 
 //    //Exercice 4 //
-   class calculerSomme{
-    constructor(valeurInitiale){
-        this.total = valeurInitiale;
-    }
-    calculer(nombre){
-        this.total += nombre;
-    }
-    afficherResultat(){
-        console.log("Resultat :" + this.total);
-    }
-   }
+//    class calculerSomme{
+//     constructor(valeurInitiale){
+//         this.total = valeurInitiale;
+//     }
+//     calculer(nombre){
+//         this.total += nombre;
+//     }
+//     afficherResultat(){
+//         console.log("Resultat :" + this.total);
+//     }
+//    }
 
-   const calculatrice = new calculerSomme (10);
-   calculatrice.calculer(5);
-   calculatrice.afficherResultat();
+//    const calculatrice = new calculerSomme (10);
+//    calculatrice.calculer(5);
+//    calculatrice.afficherResultat();
 //Exercice 5//
 
 // class Arme{
@@ -71,7 +71,7 @@
 //         console.log("Dégats de l'arme :" + this.dégats);
 //         console.log("Type de l'arme : " + this.type);
 //     }
-// }
+//     }
 // class Sort{
 //     constructor(nom, puissance, coutMana, element){
 //         this.nom = nom;
@@ -86,62 +86,75 @@
 //         console.log("Element du sort : " + this.element);
 //     }
 // }
-
+// afficherInfos();{
+//         console.log("Nom de l'arme :" + this.nom);
+//         console.log("Dégats de l'arme :" + this.dégats);
+//         console.log("Type de l'arme : " + this.type);
+//     }
 //Exercice 6//
 
-// class guerrier{
-//     constructor(nom, pointsDeVie, force, titre){
-//         this.nom = nom;
-//         this.pointsDeVie = pointsDeVie;
-//         this.force = force;
-//         this.titre = titre;
+class personnage{
+    constructor(nom, pointsdeVie, force, competence){
+        this.nom = nom;
+        this.pointsDeVie = pointsdeVie;
+        this.force = force;
+        this.competence = competence;
+    }
+}
 
-//         this.afficherInfos();
-//         console.log("Nom du guerrier :" + this.nom);
-//         console.log("Points de vie du guerrier :" + this.pointsDeVie);
-//         console.log("Force du guerrier : " + this.force);
-//         console.log("Titre du guerrier : " + this.titre);
-//     }
-// }
-// class mage{
-//     constructor(nom, pointsDeVie, force, titre){
-//         this.nom = nom;
-//         this.pointsDeVie = pointsDeVie;
-//         this.force = force;
-//         this.titre = titre;
-//     }
-//     afficherInfos(){
-//         console.log("Nom du mage :" + this.nom);
-//         console.log("Points de vie du mage :" + this.pointsDeVie);
-//         console.log("Force du mage : " + this.force);
-//         console.log("Titre du mage : " + this.titre);
-//     }
-// }
-// class archer{
-//     constructor(nom, pointsDeVie, force, titre){
-//         this.nom = nom;
-//         this.pointsDeVie = pointsDeVie;
-//         this.force = force;
-//         this.titre = titre;
-//     }
-//     afficherInfos(){
-//         console.log("Nom de l'archer :" + this.nom);
-//         console.log("Points de vie de l'archer :" + this.pointsDeVie);
-//         console.log("Force de l'archer : " + this.force);
-//         console.log("Titre de l'archer : " + this.titre);
-//     }
-// }
-// class paladin{
-//     constructor(nom, pointsDeVie, force, titre){
-//         this.nom = nom;
-//         this.pointsDeVie = pointsDeVie;
-//         this.force = force;
-//         this.titre = titre;
-//     }
-//     afficherInfos(){
-//         console.log("Nom du paladin :" + this.nom);
-//         console.log("Points de vie du paladin :" + this.pointsDeVie);
-//         console.log("Force du paladin : " + this.force);
-//         console.log("Titre du paladin : " + this.titre);
-//     }
-// }
+class guerrier{
+    constructor(nom, pointsDeVie, force, competence){
+        this.nom = nom;
+        this.pointsDeVie = pointsDeVie;
+        this.force = force;
+        this.competence = competence;
+
+        this.afficherInfos();
+        console.log("Nom du guerrier :" + this.nom);
+        console.log("Points de vie du guerrier :" + this.pointsDeVie);
+        console.log("Force du guerrier : " + this.force);
+        console.log("Compétence du guerrier : " + this.competence);
+    }
+}
+class mage{
+    constructor(nom, pointsDeVie, force, competence){
+        this.nom = nom;
+        this.pointsDeVie = pointsDeVie;
+        this.force = force;
+        this.competence = competence;
+    }
+    afficherInfos(){
+        console.log("Nom du mage :" + this.nom);
+        console.log("Points de vie du mage :" + this.pointsDeVie);
+        console.log("Force du mage : " + this.force);
+        console.log("Compétence du mage : " + this.competence);
+    }
+}
+class archer{
+    constructor(nom, pointsDeVie, force, competence){
+        this.nom = nom;
+        this.pointsDeVie = pointsDeVie;
+        this.force = force;
+        this.competence = competence;
+    }
+    afficherInfos(){
+        console.log("Nom de l'archer :" + this.nom);
+        console.log("Points de vie de l'archer :" + this.pointsDeVie);
+        console.log("Force de l'archer : " + this.force);
+        console.log("Compétence de l'archer : " + this.competence);
+    }
+}
+class paladin{
+    constructor(nom, pointsDeVie, force, competence){
+        this.nom = nom;
+        this.pointsDeVie = pointsDeVie;
+        this.force = force;
+        this.competence = competence;
+    }
+    afficherInfos(){
+        console.log("Nom du paladin :" + this.nom);
+        console.log("Points de vie du paladin :" + this.pointsDeVie);
+        console.log("Force du paladin : " + this.force);
+        console.log("Compétence du paladin : " + this.competence);
+    }
+}
